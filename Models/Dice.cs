dc0d09f60b66105174b0bf777f163613f8bd9759
hche <h.eckert@mailfence.com>
2020-01-22 20:40:08 +0100

    initial commit

---
+++ Models/Dice.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DiceRoller.Models
+{
+    public class Dice
+    {
+        private int[] rolls = new int[] { -1 };
+        private Random rnd = new Random();
+        
+        public Guid Id { get; set; }
+
+        public int Sides { get; set; }
+
+        public int Value { get { return rolls[rolls.Length - 1]; } }
+
+        public int PreviousValue
+        {
+            get
+            {
+                return rolls.Length > 1 ? rolls[rolls.Length - 2] : rolls[rolls.Length - 1];
+            }
+        }
+
+        public string Name { get { return $"D{Sides}"; } }
+
+        public Dice(int sides)
+        {
+            Id = Guid.NewGuid();
+            Sides = sides;
+        }
+
+        public int Roll()
+        {
+            var roll = rnd.Next(1, Sides + 1);
+            int[] updatedRolls = new int[rolls.Length + 1];
+            rolls.CopyTo(updatedRolls, 0);
+            updatedRolls[updatedRolls.Length - 1] = roll;
+
+            this.rolls = updatedRolls;
+            return this.Value;
+        }
+    }
+}
---
+++ Models/IDataRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DiceRoller.Models
+{
+    interface IDataRepository
+    {
+        public Dictionary<Guid, List<Dice>> getAllDiceSets();
+        public Dictionary<Guid, List<Dice>> removeDiceSet(Guid id);
+
+        public Guid addDiceSet(List<Dice> dices);
+    }
+}
